cba9f185d3cbfb5f3f027b18950e71a83d7f3da8
dong-hun <1031nice@gmail.com>
2020-05-21 20:02:18 +0900

    Spring data common - async query

--- src/main/java/me/donghun/springdatajpainflearn/CommentRepository.java
+++ src/main/java/me/donghun/springdatajpainflearn/CommentRepository.java
@@ -3,8 +3,12 @@ package me.donghun.springdatajpainflearn;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.util.concurrent.ListenableFuture;
 
 import java.util.List;
+import java.util.concurrent.Future;
 
 // 이렇게 하면 아무기능도 없다
 // 기존의 것은 빼고 제공하고 싶은 기능이 있을 때 쓸 수 있을 듯
@@ -15,6 +19,9 @@ import java.util.List;
 
 
 @RepositoryDefinition(domainClass = Comment.class, idClass = Long.class)
+//@EnableAsync // 이 애노테이션을 붙여야 async하게 동작
+// 얘를 추가해야 @Async라고 붙어있는 public 메소드들을 백그라운드 스레드풀에 넣어서 실행해준다
+// 근데 비추.
 public interface CommentRepository extends MyRepository<Comment, Long>{
 
     /*잘 만들어졌는지 확인하려면 테스트 코드를 작성해야 한다.
@@ -26,10 +33,15 @@ public interface CommentRepository extends MyRepository<Comment, Long>{
 
     List<Comment> findAll();
 
+    // 비동기 쿼리 메소드
+    @Async
+//    Future<List<Comment>> findByCommentContainsIgnoreCase(String keyword, Pageable pagable);
+    ListenableFuture<List<Comment>> findByCommentContainsIgnoreCase(String keyword, Pageable pagable);
+
     // 정적
-    List<Comment> findByCommentContainsIgnoreCaseOrderByLikeCountAsc(String title);
+//    List<Comment> findByCommentContainsIgnoreCaseOrderByLikeCountAsc(String title);
     // 동적
-    Page<Comment> findByCommentContainsIgnoreCase(String title, Pageable pageable);
+//    Page<Comment> findByCommentContainsIgnoreCase(String title, Pageable pageable);
 
     //아래 같은 것도 가능
 //    List<Comment> findByTitleContains(String keyword);
--- src/test/java/me/donghun/springdatajpainflearn/CommentRepositoryTest.java
+++ src/test/java/me/donghun/springdatajpainflearn/CommentRepositoryTest.java
@@ -8,9 +8,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,15 +26,42 @@ public class CommentRepositoryTest {
     CommentRepository commentRepository;
 
     @Test
-    public void crud(){
+    public void crud() throws ExecutionException, InterruptedException {
         this.createComment(100, "spring data jpa");
         this.createComment(55, "HIBERNATE SPRING");
 
         PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "LikeCount"));
 
-        Page<Comment> comments = commentRepository.findByCommentContainsIgnoreCase("spring", pageRequest);
-        assertThat(comments.getNumberOfElements()).isEqualTo(2);
-        assertThat(comments).first().hasFieldOrPropertyWithValue("likeCount", 100);
+        ListenableFuture<List<Comment>> future =
+                commentRepository.findByCommentContainsIgnoreCase("spring", pageRequest);
+        System.out.println("======");
+        System.out.println("is done?" + future.isDone());
+
+        future.addCallback(new ListenableFutureCallback<List<Comment>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                System.out.println(throwable);
+            }
+
+            @Override
+            public void onSuccess(List<Comment> comments) {
+                System.out.println("==== Async ====");
+                System.out.println(comments.size());
+            }
+        });
+
+        Thread.sleep(5000l);
+
+//        Future<List<Comment>> future =
+//                commentRepository.findByCommentContainsIgnoreCase("spring", pageRequest);
+//        System.out.println("=======");
+//        System.out.println("is done?" + future.isDone());
+//        List<Comment> comments = future.get(); // future get은 결과를 기다린다
+//        comments.forEach(System.out::println);
+
+//        Page<Comment> comments = commentRepository.findByCommentContainsIgnoreCase("spring", pageRequest);
+//        assertThat(comments.getNumberOfElements()).isEqualTo(2);
+//        assertThat(comments).first().hasFieldOrPropertyWithValue("likeCount", 100);
     }
 
     private void createComment(int likeCount, String comment){
